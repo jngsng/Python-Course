4390a8266c055a92e4437b72d41668426e5f7cb2
JongSeung Kim <88412551+jngsng@users.noreply.github.com>
2022-06-30 14:12:55 +0000

    type, list

--- Course.py
+++ Course.py
@@ -1,8 +1,14 @@
+# type
 a_string = "like this"
 a_number = 3
 a_float = 3.12
 a_boolean = True
 a_none = None
 
-print(type(a_string))
-print(type(a_none))
+
+# sequence type (list, tuple)
+days = "mon,Tue,Wed,Thur,Fri"
+print(days) 
+
+days = ["Mon","Tue","Wed","Thur","Fri"]
+print(days)
